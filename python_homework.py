c47176d98c3e05477b0e9f18e1c9f69ade9acfa2
tangerine0101 <sejinjenny@gmail.com>
2026-06-02 17:42:45 +0900

    20260601_dict

---
+++ python_homework.py
@@ -0,0 +1,64 @@
+# 변수
+# 1
+age = 24
+name = '권세진'
+# 2
+height = 168
+dream = '지각하지 않기'
+# 3
+city = '인천광역시'
+my_city = city
+print(my_city, city)
+# 4
+apple_count = 10
+orange_count = 20
+print(apple_count, orange_count)
+# 5
+first_name = '세진'
+last_name = '권'
+full_name = first_name + last_name
+print(full_name)
+
+#기본 자료형
+# 1
+num1 = 10
+num2 = 5
+sum_result = num1 + num2
+print(sum_result)
+
+# 2
+x = 7.5
+y = 2
+result = x / y
+print(result)
+
+# 3
+is_student = True
+print(is_student, type(is_student))
+
+# 4
+num1 = 15
+num2 = 4
+print(num1%num2, num1//num2)
+
+# 5
+greeting_message = "Hello"
+greeting_target = "World"
+print(greeting_message + greeting_target)
+
+# 6
+base = 2
+exponent = 5
+print(base ** exponent)
+
+
+
+
+
+
+
+
+
+
+
+
